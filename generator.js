eb8e9237e0bf930d3befadd71e5c882063b61711
startupandrew <startupandrew@gmail.com>
2013-01-05 22:58:07 -0800

    Initial version of work queue

---
+++ generator.js
@@ -0,0 +1,9 @@
+var Firebase = require("./firebase-node.js");
+var newWorkPeriod = 700;
+var workItems = new Firebase("https://workqueue.firebaseio-demo.com/");
+
+var i = 0;
+setInterval(function() {
+ workItems.push({number: i, time: Math.floor(Math.random()*2000)});
+ i++;
+}, newWorkPeriod);
